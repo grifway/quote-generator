ed8215a3c17fdb1cbc8ab08bad6d47cccbc982c6
grifway <grifway@yahoo.com>
2020-08-08 14:27:32 -0700

    playing 01

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // window.console.log('template');
 // or 
 console.log('template testing');
+
+
+
+// udemy course - I am jumping around
+// https://www.udemy.com/course/javascript-the-basics-for-beginners/learn/lecture/5973280#overview
+
+// object constructor function
+// build more than one instance of an object vs object literals
+// capitalize first letter of function constructor name for convention
+// this keyword refers to the instance of the object
